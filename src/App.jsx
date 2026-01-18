ffb1b0c6300a0a5d752a3b515e6baa9a902a6ce5
PierreSarciat <phsarciat@mailo.com>
2026-01-18 13:27:01 +0100

    préparation fichiers

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
-/*import Navbar from "./components/navbar/Navbar";
-import Hero from "./sections/Hero/Hero";
-import Programs from "./sections/Programs/Programs";
+import React from 'react';
+import Navbar from "./components/navbar/navbar.jsx";
+import Hero from "./sections/hero/hero.jsx";
+/*import Programs from "./sections/Programs/Programs";
 import Benefits from "./sections/Benefits/Benefits";
 import About from "./sections/About/About";
-import Contact from "./sections/Contact/Contact";
-import Footer from "./components/Footer/Footer";*/
+import Contact from "./sections/Contact/Contact";*/
+import Footer from "./components/footer/footer.jsx";
 
 function App() {
     return (
         <>
             <Navbar />
             <Hero />
-            <Programs />
-            <Benefits />
-            <About />
-            <Contact />
             <Footer />
         </>
     );
