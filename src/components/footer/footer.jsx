2aa17093557983fa91f402c10eeff4bf380039fc
PierreSarciat <phsarciat@mailo.com>
2026-01-18 13:25:23 +0100

    préparation fichiers

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+/*import '.footer.scss';*/
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer__content">
+
+                <div className="footer__section">
+                    <h3 className="footer__title">Liens utiles</h3>
+                    {/*  <ul className="footer__list">
+                        <li className="footer__item">
+                            <a href="/" className="footer__link">Accueil</a>
+                        </li>
+                        <li className="footer__item">
+                            <a href="/#programmes" className="footer__link">Programmes</a>
+                        </li>
+                        <li className="footer__item">
+                            <a href="/#about" className="footer__link">À propos</a>
+                        </li>
+                        <li className="footer__item">
+                            <a href="/#contact" className="footer__link">Contact</a>
+                        </li>
+                    </ul>*/}
+                </div>
+            </div>
+
+        </footer>
+    );
+};
+
+export default Footer;
