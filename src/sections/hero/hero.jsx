de3b19c2a86b852ad3b0c1f76c45b8ff13838273
PierreSarciat <phsarciat@mailo.com>
2026-01-18 13:26:07 +0100

    préparation fichiers

---
+++ src/sections/hero/hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './hero.scss';
+
+
+const Hero = () => {
+    return (
+        <section>
+            <div className="hero__content">
+                <h1 className="hero__title">
+                    Transformez votre carrière avec un accompagnement sur mesure
+                </h1>
+                <p className="hero__subtitle">
+                    Découvrez nos programmes pour booster votre développement professionnel et atteindre vos objectifs.
+                </p>
+
+            </div>
+        </section>
+    );
+};
+
+export default Hero;
