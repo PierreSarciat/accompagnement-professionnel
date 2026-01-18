5a073324ac1bcd4f0c7acdeef2c75ea9f9b486f3
PierreSarciat <phsarciat@mailo.com>
2026-01-18 21:29:22 +0100

    navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,10 +1,45 @@
-import React from 'react';
-/*import './navbar.scss';*/
+import React, { useState, useEffect } from "react";
+import './navbar.scss';
 
 const Navbar = () => {
+    const [scrolled, setScrolled] = useState(false);
+
+    const handleScroll = () => {
+        const scrollY = window.scrollY;
+        setScrolled(scrollY > 0);
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
 
     return (
-        <p>Delphine Rivier</p>
+        <div className={`navbar-container ${scrolled ? 'scrolled' : ''}`}>
+            <nav>
+                <ul>
+                    <li>
+                        <div className="nav1">
+                            <a href="#portfolio">Portfolio</a>
+                        </div>
+                    </li>
+                    <li className="nav2">
+                        <a href="#competences">Compétences</a>
+                    </li>
+                    <li className="nav2">
+                        <a href="#about">À propos</a>
+                    </li>
+                    <li className="nav2">
+                        <a href="#projets">Projets</a>
+                    </li>
+                    <li className="nav2">
+                        <a href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     );
 };
 
