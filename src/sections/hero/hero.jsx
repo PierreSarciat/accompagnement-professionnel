e20893d43f8b800ec9bd33f809cf3021bf3c0749
PierreSarciat <phsarciat@mailo.com>
2026-01-21 19:35:21 +0100

    mise en place section hero

--- src/sections/hero/hero.jsx
+++ src/sections/hero/hero.jsx
@@ -2,19 +2,27 @@ import React from 'react';
 import './hero.scss';
 
 
+
 const Hero = () => {
     return (
-        <section className='section'>
-            <div className="hero__content">
-                <h1 className="hero__title">
-                    Votre avenir professionnel commence ici. Transformez votre vie dès maintenant !
-                </h1>
-                <p className="hero__subtitle">
-                    Avec un accompagnement personnalisé, atteignez vos objectifs professionnels plus vite que vous ne l’imaginez.
-                </p>
+        <div className="herobackground">
+            <section className='section hero'>
+                <div className="hero__content">
+                    <div className='colonne1'>
+                        <h1 className="hero__title">
+                            Votre avenir professionnel commence ici. Transformez votre vie dès maintenant !
+                        </h1>
+                        <p className="hero__subtitle">
+                            Avec un accompagnement personnalisé, atteignez vos objectifs professionnels plus vite que vous ne l’imaginez.
+                        </p>
+                    </div>
+                    <div className='colonne2'></div>
+                    <div className='colonne3'></div>
 
-            </div>
-        </section>
+                </div>
+            </section>
+        </div>
+
     );
 };
 
