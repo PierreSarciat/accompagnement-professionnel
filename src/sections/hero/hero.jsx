ab65acbcf2f0ba26d20a907e42d42a6eaabcc080
PierreSarciat <phsarciat@mailo.com>
2026-01-22 23:12:35 +0100

    mise en place hero

--- src/sections/hero/hero.jsx
+++ src/sections/hero/hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './hero.scss';
+import Button from "@components/button/button.jsx";
 
 
 
@@ -15,9 +16,16 @@ const Hero = () => {
                         <p className="hero__subtitle">
                             Avec un accompagnement personnalisé, atteignez vos objectifs professionnels plus vite que vous ne l’imaginez.
                         </p>
+                        <div className='button-container'>
+                            <Button text="A propos" href="" />
+                            <Button text="Contactez-moi" href="" />
+                        </div>
+
                     </div>
-                    <div className='colonne2'></div>
-                    <div className='colonne3'></div>
+                    <div className='colonne2'>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eget lacus vitae nulla varius feugiat id non est. Ut ut velit dignissim, maximus massa eget, luctus velit. Cras vel odio non orci maximus scelerisque. In lobortis lacus lectus, sed ultricies odio maximus sit amet. Aliquam id facilisis arcu, vel iaculis urna. Cras aliquam arcu ex, mattis mattis nisl porta dapibus. </p>
+                    </div>
+
 
                 </div>
             </section>
