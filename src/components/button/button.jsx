756a279beed5d91fea2ccae699d50c27fa3c7e07
PierreSarciat <phsarciat@mailo.com>
2026-01-28 19:31:50 +0100

    routage page contact

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,14 +1,27 @@
-import React from "react";
-import "./button.scss";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './button.scss';
 
-const Button = ({ text, href, ...props }) => {
-
-
-    return (
-        <a className="button" href={href} {...props}>
-            {text}
-        </a>
-    );
+const Button = ({ text, to, href, onClick, type = 'button', isLink = false, isRouterLink = false }) => {
+    if (isRouterLink) {
+        return (
+            <Link to={to} className="button" onClick={onClick}>
+                {text}
+            </Link>
+        );
+    } else if (isLink) {
+        return (
+            <a className="button" href={href} onClick={onClick}>
+                {text}
+            </a>
+        );
+    } else {
+        return (
+            <button className="button" type={type} onClick={onClick}>
+                {text}
+            </button>
+        );
+    }
 };
 
 export default Button;
