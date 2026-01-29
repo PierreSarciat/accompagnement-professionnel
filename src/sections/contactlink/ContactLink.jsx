8bf2b73405879cbdeeb824170d6732043b8cc402
PierreSarciat <phsarciat@mailo.com>
2026-01-29 21:44:02 +0100

    ajouts Contactlink

--- src/sections/contactlink/ContactLink.jsx
+++ src/sections/contactlink/ContactLink.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import Button from "@components/button/button.jsx";
 import { Link } from 'react-router-dom';
+import './ContactLink.scss';
 
 const ContactLink = () => {
     return (
-        <div className='' id="contact">
-            <p>Vous préparer seul est possible.
-                Être accompagné change tout.</p>
-
-            <p>Discutons ensemble de votre projet et de vos objectifs lors d'un premier entretien gratuit.</p>
-            <Link to="/contact">
-                <Button text="Contactez-moi" href="" />
-            </Link>
-        </div>
+        <section>
+            <div className='ContactLink__content' id="contact">
+                <h1>Vous préparer seul est possible.
+                    Être accompagné change tout.</h1>
+                <p>Discutons ensemble de votre projet et de vos objectifs lors d'un premier entretien gratuit.</p>
+                <Link to="/contact">
+                    <Button text="Contactez-moi" />
+                </Link>
+            </div>
+        </section>
 
     );
 };
